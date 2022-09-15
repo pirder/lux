56f3f95df1f22104ed8c740efd5b0936c36607fb
priders <priders@zyh.com>
2022-09-15 19:57:11 +0800

    新增：新增脚本、makefile 支持编译移动平台

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"C"
 	"fmt"
-	"os"
-
 	"github.com/fatih/color"
-
 	"github.com/iawia002/lux/app"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -18,4 +18,18 @@ func main() {
 		)
 		os.Exit(1)
 	}
+}
+
+//export cli
+func cli(in *C.char) *C.char {
+	var arg = C.GoString(in)
+	var args = strings.Split(arg, ",")
+	var errStr = ""
+	if err := app.New().Run(args); err != nil {
+		errStr = fmt.Sprintf(
+			"Run %s failed: %s\n",
+			color.CyanString("%s", app.Name), color.RedString("%v", err),
+		)
+	}
+	return C.CString(errStr)
 }
